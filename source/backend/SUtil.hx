32fbcbf1b5986cb31bd95a50d48635057a1a4be3
Lily <egzozu.be.bas@gmail.com>
2023-11-24 19:49:23 +0300

    guh

--- source/backend/SUtil.hx
+++ source/backend/SUtil.hx
@@ -38,7 +38,7 @@ class SUtil
 					@:privateAccess
 					File.saveBytes(fixedPath, cast LimeAssets.getAsset(LimeAssets.libraryPaths.exists(library) ? '$library:$file' : file, getFileType(Path.withoutDirectory(file)), false));
 				} catch(error:Dynamic) {
-					Toast.makeText('Error!\nClouldn\'t copy $file because:\n' + error, Toast.LENGTH_LONG);
+					#if (android && debug) Toast.makeText("Error!\nClouldn't copy the file because:\n" + error, Toast.LENGTH_LONG); #else LimeLogger.println("Error!\nClouldn't copy the file because:\n" + error); #end
 				}
 			}
 		}
