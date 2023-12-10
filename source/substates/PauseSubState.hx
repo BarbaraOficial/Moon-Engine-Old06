c29929dd3369ce48535bae854dd1889639c93690
Lily <egzozu.be.bas@gmail.com>
2023-12-10 19:56:58 +0300

    _instance_

--- source/substates/PauseSubState.hx
+++ source/substates/PauseSubState.hx
@@ -266,7 +266,7 @@ class PauseSubState extends MusicBeatSubstate
 				case "Restart Song":
 					restartSong();
 				case 'Chart Editor':
-					PlayState.openChartEditor();
+					PlayState.instance.openChartEditor();
 				case "Leave Charting Mode":
 					restartSong();
 					PlayState.chartingMode = false;
