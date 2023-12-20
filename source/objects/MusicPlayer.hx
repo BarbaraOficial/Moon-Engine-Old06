5cd9934ba9a7599e2d094f423e61d74fd58563e0
mcgagabe19 <egzozu.be.bas@gmail.com>
2023-12-20 21:26:12 +0300

    reset button

--- source/objects/MusicPlayer.hx
+++ source/objects/MusicPlayer.hx
@@ -199,7 +199,7 @@ class MusicPlayer extends FlxGroup
 		}
 		updatePlaybackTxt();
 	
-		if (FlxG.keys.pressed.R)
+		if (instance.virtualPad.buttonC.justPressed || FlxG.keys.pressed.R)
 		{
 			playbackRate = 1;
 			setPlaybackRate();
@@ -250,7 +250,7 @@ class MusicPlayer extends FlxGroup
 		if (playingMusic)
 		{
 			if (ClientPrefs.data.controlsAlpha >= 0.1)
-				instance.bottomText.text = "Press X to Pause / Press B to Exit / Press ? to Reset the Song";
+				instance.bottomText.text = "Press X to Pause / Press B to Exit / Press C to Reset the Song";
 			else
 				instance.bottomText.text = "Press SPACE to Pause / Press ESCAPE to Exit / Press R to Reset the Song";
 			positionSong();
