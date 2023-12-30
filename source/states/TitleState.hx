70b5094d6976b53816fd2fca26bc4e656bbf5b04
BarbaraOrigami <138633318+BarbaraOrigami@users.noreply.github.com>
2023-12-30 09:36:34 -0300

    Update TitleState.hx

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -116,7 +116,7 @@ class TitleState extends MusicBeatState
 		#if CHECK_FOR_UPDATES
 		if(ClientPrefs.data.checkForUpdates && !closedState) {
 			trace('checking for update');
-			var http = new haxe.Http("https://raw.githubusercontent.com/BarbaraOrigami/Moon-Engine-New/main/gitVersion.txt");
+			var http = new haxe.Http("https://raw.githubusercontent.com/BarbaraOrigami/Moon-Engine/main/gitVersion.txt");
 
 			http.onData = function (data:String)
 			{
