772662bea1e8d32e8c460c73a34abbc2e03e0f4e
BarbaraOrigami <138633318+BarbaraOrigami@users.noreply.github.com>
2023-12-30 11:12:21 -0300

    Update CreditsState.hx

--- source/states/CreditsState.hx
+++ source/states/CreditsState.hx
@@ -41,7 +41,7 @@ class CreditsState extends MusicBeatState
 		var defaultList:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
 			['Moom Engine Team'],
 			['BarbaraOficial',		                'barbaraoficial',		                'Main Programmer and leader of Moon Engine',							'https://twitter.com/Barbara_Origami',           'D00055'],
-			['DaricrahDev',				'daricrahdev',			'Programmer Helping at Moon Engine',						'https://twitter.com/Daricrah',		'FDE870']
+			['DaricrahDev',				'daricrahdev',			'Programmer Helping at Moon Engine',						'https://twitter.com/Daricrah',		'FDE870'],
 			[''],
 			['Mobile Porting Team (Psych Engine Android)'],
 			['mcagabe19',		                'lily',		                'Head Porter of Psych Engine Mobile',							'https://www.youtube.com/@mcagabe19',           'FFE7C0'],
