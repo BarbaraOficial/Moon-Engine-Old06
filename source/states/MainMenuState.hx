0ce9e923904ac0cb2009cb5bfea692b8b85c26d8
BarbaraOrigami <138633318+BarbaraOrigami@users.noreply.github.com>
2023-12-30 19:55:41 -0300

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -72,7 +72,7 @@ class MainMenuState extends MusicBeatState
 		for (i in 0...optionShit.length)
 		{
 			var offset:Float = 108 - (Math.max(optionShit.length, 4) - 4) * 80;
-			var menuItem:FlxSprite = new FlxSprite(0, (i * 100) + offset);
+			var menuItem:FlxSprite = new FlxSprite(0, (i * 120) + offset);
 			menuItem.antialiasing = ClientPrefs.data.antialiasing;
 			menuItem.frames = Paths.getSparrowAtlas('mainmenu/menu_' + optionShit[i]);
 			menuItem.animation.addByPrefix('idle', optionShit[i] + " basic", 24);
