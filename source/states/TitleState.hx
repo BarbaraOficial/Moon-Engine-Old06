472102094ae2629926c52bbd6163da38b652625f
BarbaraOficial <138633318+BarbaraOficial@users.noreply.github.com>
2024-01-01 19:43:45 -0300

    Update TitleState.hx

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -441,7 +441,14 @@ class TitleState extends MusicBeatState
 					if (mustUpdate) {
 						MusicBeatState.switchState(new OutdatedState());
 					} else {
-						MusicBeatState.switchState(new MainMenuState());
+						switch (ClientPrefs.data.menuType){
+						
+							case 'Moon Engine':
+								MusicBeatState.switchState(new MainMenuState());
+							
+							case 'Psych Engine':
+								MusicBeatState.switchState(new MainMenuPsychState());
+						}
 					}
 					closedState = true;
 				});
