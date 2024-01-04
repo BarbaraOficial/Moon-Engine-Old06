5465f7c68ea3d4d70d84b11ae42e57f130accaec
BarbaraOficial <138633318+BarbaraOficial@users.noreply.github.com>
2024-01-04 17:56:03 -0300

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -78,7 +78,7 @@ class MainMenuState extends MusicBeatState
 		mainSide.updateHitbox();
 		mainSide.screenCenter();
 		mainSide.antialiasing = ClientPrefs.data.antialiasing;
-		mainSide.x = -200;
+		mainSide.x = -100;
 		mainSide.y = -90;
 		add(mainSide);
 
