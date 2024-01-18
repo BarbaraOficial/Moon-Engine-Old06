65d7ea7ff2706642aa79010b54b02c3734e88ebe
BarbaraOficial <138633318+BarbaraOficial@users.noreply.github.com>
2024-01-18 14:03:58 -0300

    Update Section.hx

--- source/backend/Section.hx
+++ source/backend/Section.hx
@@ -4,7 +4,6 @@ typedef SwagSection =
 {
 	var sectionNotes:Array<Dynamic>;
 	var sectionBeats:Float;
-	var typeOfSection:Int;
 	var mustHitSection:Bool;
 	var gfSection:Bool;
 	var bpm:Float;
@@ -18,7 +17,6 @@ class Section
 
 	public var sectionBeats:Float = 4;
 	public var gfSection:Bool = false;
-	public var typeOfSection:Int = 0;
 	public var mustHitSection:Bool = true;
 
 	public function new(sectionBeats:Float = 4)
