fd0bc19a10ee721ab304baf3b33e5b04fac25b45
BarbaraOficial <138633318+BarbaraOficial@users.noreply.github.com>
2024-01-18 13:25:41 -0300

    Add files via upload

--- source/options/MobileOptionsSubState.hx
+++ source/options/MobileOptionsSubState.hx
@@ -2,26 +2,34 @@ package options;
 
 class MobileOptionsSubState extends BaseOptionsMenu
 {
-	var mafaka:Float;
 	public function new()
 	{
 		title = 'Mobile Options';
 		rpcTitle = 'Mobile Options Menu'; //for Discord Rich Presence, fuck it
 
-                #if !web
-		var option:Option = new Option('Extra Controls', //Name
-			'Select how many extra buttons you prefere to have\nThey can be used for mechanics with LUA or HScript.', //Description
-			'extraButtons', //Save data variable name
-			'string',
-			["NONE", "ONE", "TWO"]); //Variable type
+		#if mobile
+		var option:Option = new Option('Allow Phone Screensaver',
+		'If checked, the phone will sleep after going inactive for few seconds',
+		'screensaver',
+		'bool');
+		option.onChange = () -> {
+			lime.system.System.allowScreenTimeout = curOption.getValue();
+		};
 		addOption(option);
-
-        var option:Option = new Option('Hitbox Position', //Name
+		#end
+			
+		var option:Option = new Option('Hitbox Position', //Name
 			'If checked, the hitbox will be put at the bottom of the screen, otherwise will stay at the top.', //Description
 			'hitbox2', //Save data variable name
 			'bool'); //Variable type
 		addOption(option);
-                #end
+
+		var option:Option = new Option('Extra Controls', //Name
+		'Select how many extra buttons you prefere to have\nThey can be used for mechanics with LUA or HScript.', //Description
+		'extraButtons', //Save data variable name
+		'string',
+		["NONE", "ONE", "TWO"]); //Variable type
+		addOption(option);
 
 		var option:Option = new Option('Dynamic Controls Color',
 		'If checked, the mobile controls color will be set to the notes color in your settings.\n(have effect during gameplay only)',
@@ -38,15 +46,11 @@ class MobileOptionsSubState extends BaseOptionsMenu
 		option.maxValue = 1;
 		option.changeValue = 0.1;
 		option.decimals = 1;
-		option.onChange = setAlpha;
+		option.onChange = () -> {
+			virtualPad.alpha = curOption.getValue();
+		};
 		addOption(option);
 
 		super();
-	}
-	public function setAlpha():Void {
-		if (curOption.getValue() != mafaka)
-			mafaka = curOption.getValue();
-
-		virtualPad.alpha = mafaka;
 	}
 }
