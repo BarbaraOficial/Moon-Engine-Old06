b1b6811cd21b5471c02e2342c49fd889a716d18d
BarbaraOficial <138633318+BarbaraOficial@users.noreply.github.com>
2024-01-18 16:54:36 -0300

    Update OptionsState.hx

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -84,8 +84,7 @@ class OptionsState extends MusicBeatState
 		{
 			var optionText:Alphabet = new Alphabet(0, 0, options[i], true);
 			optionText.screenCenter();
-			optionText.y += (80 * (i - (options.length / 2))) + 50;
-			optionText.x += 1000;
+			optionText.y += (100 * (i - (options.length / 2))) + 50;
 			optionText.alignment = RIGHT;
 			grpOptions.add(optionText);
 		}
