7fe6d8fb34b368d546a392527b58920ecced76b1
BarbaraOficial <138633318+BarbaraOficial@users.noreply.github.com>
2024-01-21 15:40:48 -0300

    Add files via upload

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -2117,7 +2117,7 @@ class PlayState extends MusicBeatState
 	  else if (healthBar.percent < 20) {
 		iconP2.animation.curAnim.curFrame = 2;
 		}
-	  else {
+	  else
 	    iconP2.animation.curAnim.curFrame = 0;
 	}
 	else {
