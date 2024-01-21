d8fe5c2d9c8ea4f9f1f106cb9d6bba46573471b1
BarbaraOficial <138633318+BarbaraOficial@users.noreply.github.com>
2024-01-21 16:51:19 -0300

    Add files via upload

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -2123,9 +2123,9 @@ if (iconP2.animation.frames == 3) {
 	    iconP2.animation.curAnim.curFrame = 0;
     }
    }
-else if (iconP2.animation.frames == 2) {
+else {
 	    iconP2.animation.curAnim.curFrame = (healthBar.percent > 80) ? 1 : 0; //If health is over 80%, change opponent icon to frame 1 (losing icon), otherwise, frame 0 (normal)
-  }
+}
 }
 		return health;
 	}
