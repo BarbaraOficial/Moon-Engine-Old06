5f729ef6b464053977d038d4ae8f909e991c91ee
BarbaraOficial <138633318+BarbaraOficial@users.noreply.github.com>
2024-01-21 15:35:20 -0300

    Add files via upload

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -2105,9 +2105,9 @@ class PlayState extends MusicBeatState
 	  else
 	    iconP1.animation.curAnim.curFrame = 0;
 	}
-}
 	else {
 	  iconP1.animation.curAnim.curFrame = (healthBar.percent < 20) ? 1 : 0; //If health is under 20%, change player icon to frame 1 (losing icon), otherwise, frame 0 (normal)
+   }
 }
 	
 	if (iconP2.animation.frames == 3) {
@@ -2120,9 +2120,9 @@ class PlayState extends MusicBeatState
 	  else {
 	    iconP2.animation.curAnim.curFrame = 0;
 	}
-}
 	else {
 	iconP2.animation.curAnim.curFrame = (healthBar.percent > 80) ? 1 : 0; //If health is over 80%, change opponent icon to frame 1 (losing icon), otherwise, frame 0 (normal)
+  }
 }
 	   
 		return health;
