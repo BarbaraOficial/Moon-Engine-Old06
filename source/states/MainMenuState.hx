2e072c143ccf70e811113a52548d3695fa223d42
BarbaraOficial <138633318+BarbaraOficial@users.noreply.github.com>
2024-01-23 20:12:09 -0300

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -9,7 +9,7 @@ import options.OptionsState;
 
 class MainMenuState extends MusicBeatState
 {
-	public static var moonEngineVersion:String = '2.0.1'; // This is also used for Discord RPC
+	public static var moonEngineVersion:String = '2.0.1k'; // This is also used for Discord RPC
 	public static var psychEngineVersion:String = '0.7.3';
 	public static var curSelected:Int = 0;
 
